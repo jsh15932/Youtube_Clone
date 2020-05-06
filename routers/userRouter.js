46ee5e4442ac027a1fd759d6d2d0c2b8aba969e4
장승훈 <jsh1400512@naver.com>
2020-05-07 04:50:27 +0900

    Commit

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,11 +3,11 @@ import routes from "../routes";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, (req, res) => res.send("Users"));
-userRouter.get(routes.userDetail, (req, res) => res.send("User Detail"));
-userRouter.get(routes.editProfile, (req, res) => res.send("Edit Profile"));
+userRouter.get(routes.users, (req, res) => res.send("users"));
+userRouter.get(routes.userDetail, (req, res) => res.send("userDetail"));
+userRouter.get(routes.editProfile, (req, res) => res.send("editProfile"));
 userRouter.get(routes.changePassword, (req, res) => 
-    res.send("Change Password")
+    res.send("changePassword")
 );
 
 export default userRouter;
